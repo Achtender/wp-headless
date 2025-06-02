d0a0f5d3457e38e5f4916cda7d320a22cbcd981f
Milo <201960176+milo-at-achtender@users.noreply.github.com>
2025-06-02 15:57:58 +0200

    Update _app.tsx

--- blog/routes/_app.tsx
+++ blog/routes/_app.tsx
@@ -8,6 +8,7 @@ export default function App({ Component }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>My Fresh Project</title>
         <link rel="stylesheet" href="/styles.css" />
+        <meta http-equiv="Content-Security-Policy" content="img-src 'self' mhs.achtender.eu;" />
       </head>
       <body>
         <Component />
