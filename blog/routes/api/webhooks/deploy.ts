fe7537c1866dc90f951668e26ca662d79e7ed16b
Milo <201960176+milo-at-achtender@users.noreply.github.com>
2025-06-03 09:10:09 +0200

    Testing form data

--- blog/routes/api/webhooks/deploy.ts
+++ blog/routes/api/webhooks/deploy.ts
@@ -16,26 +16,43 @@ export const handler: Handlers = {
       });
     }
 
-    const octokit = new Octokit({
-      auth: GITHUB_TOKEN,
-    });
+    let data;
+    let rawBody = await req.text();
+    try {
+      data = JSON.parse(rawBody);
+      console.log("Request JSON body:", data);
+    } catch {
+      data = rawBody;
+      console.log("Request raw body:", data);
+    }
 
-    const response = await octokit.request(
-      "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
-      {
-        owner: "milo-at-achtender",
-        repo: "headless-wordpress",
-        workflow_id: "deploy.yml", // or replace with the actual workflow file name or numeric ID
-        ref: "main",
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28",
-        },
-      },
-    );
+    // const octokit = new Octokit({
+    //   auth: GITHUB_TOKEN,
+    // });
+
+    // const response = await octokit.request(
+    //   "POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches",
+    //   {
+    //     owner: "milo-at-achtender",
+    //     repo: "headless-wordpress",
+    //     workflow_id: "deploy.yml", // or replace with the actual workflow file name or numeric ID
+    //     ref: "main",
+    //     headers: {
+    //       "X-GitHub-Api-Version": "2022-11-28",
+    //     },
+    //   },
+    // );
+
+    // return Response.json({
+    //   status: response.status,
+    //   message: response.status === 204
+    //     ? "Deployment triggered successfully"
+    //     : "Failed to trigger deployment",
+    // });
 
     return Response.json({
-      status: response.status,
-      message: response.status === 204
+      status: 204,
+      message: 204 === 204
         ? "Deployment triggered successfully"
         : "Failed to trigger deployment",
     });
