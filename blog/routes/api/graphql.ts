04a5c1c84ec880a1446c97979a262cf0cfc61333
Milo <201960176+milo-at-achtender@users.noreply.github.com>
2025-06-16 17:00:02 +0200

    Update graphql.ts

--- blog/routes/api/graphql.ts
+++ blog/routes/api/graphql.ts
@@ -3,20 +3,24 @@
 import { Handlers } from "fresh/server.ts";
 import { gql, request } from 'graphql-request'
 
-
-
-
 export const handler: Handlers = {
   async GET(_req) {
-    const document = gql`
+    const data = await request('https://mhs.achtender.eu/wp-headless/wp-graphql/', gql`
         query {
             generalSettings {
                 title
                 description
             }
         }
-    `
-    const data = await request('https://mhs.achtender.eu/wp-headless/wp-graphql/', document)
+    `)
+    // const data = await request('https://mhs.achtender.eu/wp-headless/wp-graphql/', gql`
+    //     query {
+    //         page(id: "home" idType: URI) {
+    //             title
+    //             description
+    //         }
+    //     }
+    // `)
 
     return Response.json(data);
   },
