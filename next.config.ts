0237ca909d7b1b0ad0f1e1e6ba7f11f33ab8bc37
Milo <201960176+milo-at-achtender@users.noreply.github.com>
2025-06-24 17:15:38 +0200

    fix: build and deploy

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   output: 'standalone',
   devIndicators: false,
+  env: {
+    WORDPRESS_URL: process.env.WORDPRESS_URL,
+    WORDPRESS_HOSTNAME: process.env.WORDPRESS_HOSTNAME,
+    WORDPRESS_API_BASIC_AUTH: process.env.WORDPRESS_API_BASIC_AUTH,
+    WORDPRESS_WEBHOOK_SECRET: process.env.WORDPRESS_WEBHOOK_SECRET,
+  },
   images: {
     remotePatterns: [
       {
